c1f54f645e3a5f3c7798fa365ce0eb368b9d2d28
ltrii <lelandtr@gmail.com>
2019-01-30 13:25:38 -0800

    More reducer work

--- friends/src/reducers/friendReducer.js
+++ friends/src/reducers/friendReducer.js
@@ -1,3 +1,13 @@
+import { FETCHING_FRIENDS,
+         FRIENDS_FETCHED,
+         FRIENDS_SAVED,
+         SAVING_FRIENDS,
+         UPDATING_FRIEND,
+         FRIEND_UPDATED,
+         DELETING_FRIEND,
+         FRIEND_DELETED
+         } from '../actions';
+
 const initialState = {
     fetchingFriends: false,
     friendsFetched: false,
@@ -9,4 +19,42 @@ const initialState = {
     friendDeleted: false,
     friends: [],
     error: null
+}
+
+export const friendReducer = (state = initialState, action) => {
+    console.log('reducer', action);
+        case FETCHING_FRIENDS:
+            console.log('fetching_friends')
+            return {
+                ...state,
+                error: '',
+                isFetching: true
+            };
+        case FRIENDS_FETCHED:
+            return {
+                ...state,
+                friends: action.payload,
+                isFetching: false,
+                error: ''
+            };
+        case FRIENDS_SAVED:
+            return {
+
+            };
+        case SAVING_FRIENDS:
+            return {
+
+            };
+        case UPDATING_FRIEND:
+            return {
+
+            };
+        case FRIEND_UPDATED:
+            return {
+
+            };
+        case DELETING_FRIEND:
+            return {
+
+            };
 }
